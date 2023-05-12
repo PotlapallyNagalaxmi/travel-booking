21e81a1e368caf4d98e449d115d9f00b0c3f0c24
Nagalaxmi24 <nagalaxmi.potlapally@gmail.com>
2023-05-12 20:31:46 +0530

    change app.css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./app.css"
+import "./App.css"
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Main from './components/Main/Main';
